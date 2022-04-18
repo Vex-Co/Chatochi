f71c8f45f46b3333d28e9d702b71500fffe5c387
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-04-18 17:56:54 +0500

    Integrating Socket.io

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'views'));
+app.use(express.static('public'))
 
 app.get('', (req, res) => {
     res.render('home')
 })
 
+// IO
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
 
-module.exports = app
+module.exports = server
