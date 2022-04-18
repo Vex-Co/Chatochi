d72487189a4b01cd9216539c894aaaed6b7c7e73
Zafeer Hafeez <mrh4ck3r1738@gmail.com>
2022-04-18 17:27:52 +0500

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('', (req, res) => {
+    res.render('home')
+})
+
+
+module.exports = app
